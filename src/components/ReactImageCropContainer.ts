4a5a92892a243255903dceacc927f0fe72ee9c8d
samuelrwabwera <rwabwerakankunda@gmail.com>
2018-10-02 10:45:41 +0300

    restructure XML file

--- src/components/ReactImageCropContainer.ts
+++ src/components/ReactImageCropContainer.ts
@@ -1,14 +1,18 @@
 import { Component, createElement } from "react";
 import ReactImageCrop from "./ReactImageCrop";
 
-export interface ReactImageCropContainerProps {
-  sampleText: string;
+interface WrapperProps {
+    mxObject?: mendix.lib.MxObject;
+    mxform?: mxui.lib.form._FormBase;
+}
+export interface ReactImageCropContainerProps extends WrapperProps {
+//   editable: default| never;
+  maxWidth: number;
+  minHeight: number;
 }
 
 export default class ReactImageCropContainer extends Component<ReactImageCropContainerProps, {}> {
   render() {
-      return createElement(ReactImageCrop, {
-          sampleText: this.props.sampleText
-      });
+      return createElement(ReactImageCrop);
   }
 }
