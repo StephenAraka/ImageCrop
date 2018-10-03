490e0269e074d121ed0a1ed4e72cfe8892e484f1
samuelrwabwera <rwabwerakankunda@gmail.com>
2018-10-03 10:16:18 +0300

    update XML structure

--- src/components/ImageCropContainer.ts
+++ src/components/ImageCropContainer.ts
@@ -9,6 +9,10 @@ interface WrapperProps {
 
 export interface ImageCropContainerProps extends WrapperProps {
   dataSource: DataSource;
+  dataSourceMicroflow: string;
+  imagesEntity: string;
+  entityConstraint: string;
+  urlAttribute: string;
   imageSource: imageSourceType;
   editable: editableType;
   minWidth: number;
@@ -32,7 +36,36 @@ type PageLocation = "content" | "popup" | "modal";
 type DataSource = "static" | "XPath" | "microflow";
 
 export default class ImageCropContainer extends Component<ImageCropContainerProps, ImageCropContainerState> {
-  render() {
+    // private subscriptionHandle: number;
+    constructor(props: ImageCropContainerProps) {
+        super(props);
+
+        const alertMessage = ImageCropContainer.validateProps(props);
+        this.state = {
+            alertMessage
+        };
+    }
+
+    render() {
       return createElement(ImageCrop);
   }
+
+//     componentWillUnmount() {
+//     if (this.subscriptionHandle) window.mx.data.unsubscribe(this.subscriptionHandle);
+// }
+
+    public static validateProps(props: ImageCropContainerProps): string {
+    let message = "";
+    if (props.dataSource === "static" && !props.staticImages) {
+        message = "For the data source option 'Static', at least one static image should be added";
+    }
+    if (props.dataSource === "XPath" && !props.dataSource) {
+        message = "For the data source 'XPath', the images entity is required";
+    }
+    if (props.dataSource === "microflow" && !props.onClickMicroflow) {
+        message = "For data source option 'microflow', a data source microflow is required";
+    }
+
+    return message;
+}
 }
